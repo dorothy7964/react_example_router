e82722ef8f7a4debe9fce2e8c7cc8a340e7486a2
SEC\user <dorothy_007@naver.com>
2019-05-27 21:10:27 +0900

    NavLink 사용

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
   render(){
 
     return(
-      <div>
-    		Header
+      <div className="header">
+    		<NavLink exact to="/" className="item" activeClassName="on">홈</NavLink>
+    		<NavLink to="/about/hanpixel" className="item" activeClassName="on">소개</NavLink>
+    		<NavLink to="/posts" className="item" activeClassName="on">블로그</NavLink>
+    		<NavLink to="/Login" className="item" activeClassName="on">로그인</NavLink>
+    		<NavLink to="/Mypage" className="item" activeClassName="on">마이페이지</NavLink>
 	   </div>
     )
   }
