21453e6c040f602fc12c03c2093632d960db9652
SEC\user <dorothy_007@naver.com>
2019-05-27 21:29:04 +0900

    Posts - Link 사용

--- src/route/Posts.js
+++ src/route/Posts.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import '../css/style.css';
 
 class Posts extends Component {
 	render(){
 		return(
 			<div>
 				<h1>Posts</h1>
+				<Link to="/posts/react" className="link">react</Link>
+				<Link to="/posts/redux" className="link">redux</Link>
+				<Link to="/posts/store" className="link">store</Link>
 			</div>
 		)
 	}
