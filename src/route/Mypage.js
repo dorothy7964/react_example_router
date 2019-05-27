3deaad3834ea04fff3bb1b3a7619a8dd52014c9a
SEC\user <dorothy_007@naver.com>
2019-05-27 21:36:28 +0900

    Redirect 사용

--- src/route/Mypage.js
+++ src/route/Mypage.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
+import { Redirect } from 'react-router-dom';
+
+const logged = false;
 
 class Mypage extends Component {
 	render(){
 		return(
 			<div>
+				{ !logged && <Redirect to='/' /> }
 				<h1>Mypage</h1>
 			</div>
 		)
