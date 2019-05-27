124bb4aaaf1b01875a06908cdef612fe2eb645e7
SEC\user <dorothy_007@naver.com>
2019-05-27 21:20:11 +0900

    history.push

--- src/route/Home.js
+++ src/route/Home.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
 
 class Home extends Component {
+	movePage = () => {
+		console.log(this.props.history);
+		this.props.history.push('/about/btn');
+	}
+
 	render(){
 		return(
 			<div>
 				<h1>Home</h1>
+				<button onClick={this.movePage}>소개페이지로 이동</button>
 			</div>
 		)
 	}
